882ac19c40ec17d00946c457a08d1f5d2f52b972
Guillermo Cabana <gcabana@senati.pe>
2020-11-27 20:03:34 -0500

    20201127_heroku

--- build/src/database/conection.js
+++ build/src/database/conection.js
@@ -9,11 +9,11 @@ const dotenv_1 = __importDefault(require("dotenv"));
 function dbSinfo() {
     dotenv_1.default.config();
     return new pg_1.Pool({
-        host: "localhost",
-        user: "postgres",
-        password: "1234nix",
-        database: "blackboard",
-        //connectionString:'postgres://ltwjmfnyompypj:448d691b2faa4230c2dd5cd5d87280e3867909087c1e3ba89c597f86fe9df4f0@ec2-54-157-234-29.compute-1.amazonaws.com:5432/da84omcj3t8lf4',
+        /*  host: "localhost",//process.env.DB_HOST || 'ec2-54-157-234-29.compute-1.amazonaws.com',
+          user:  "postgres",//process.env.DB_USER || 'ltwjmfnyompypj',
+          password: "1234nix",// process.env.DB_PSWD || '448d691b2faa4230c2dd5cd5d87280e3867909087c1e3ba89c597f86fe9df4f0',//
+          database: "blackboard",//process.env.DB_DDBB || 'da84omcj3t8lf4',
+          */ connectionString: 'postgres://ltwjmfnyompypj:448d691b2faa4230c2dd5cd5d87280e3867909087c1e3ba89c597f86fe9df4f0@ec2-54-157-234-29.compute-1.amazonaws.com:5432/da84omcj3t8lf4',
         max: 10,
         min: 0,
         idleTimeoutMillis: 10000,
